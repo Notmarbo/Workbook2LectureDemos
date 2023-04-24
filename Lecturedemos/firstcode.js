0e699ef86ab0010a1c869400602023286a43ab4f
Marco Sanchez <Notmarcosan@gmail.com>
2023-04-24 15:17:30 -0400

    JS practice

---
+++ Lecturedemos/firstcode.js
@@ -0,0 +1,23 @@
+//the following is a script 
+//that does a few things
+/*
+Hello
+*/
+var firstName;
+
+firstName = "Marco";
+
+var cityName;
+
+cityName = "Forest Park "
+
+var state;
+
+state = "Georgia"
+
+var gretting;
+
+gretting = "Your name is " + firstName + " you are from " + cityName + state; //This line will asign the result of an expression to the greeting variable
+
+console.log(gretting)
+
