0434bf6202a69d7118a01e7c4796d55644bf6f3f
Marco Sanchez <Notmarcosan@gmail.com>
2023-04-25 09:54:36 -0400

    Examples completed

---
+++ Lecturedemos/2-6.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var costOfVetVisit = 70.0;
+var vacinationFee = 37.0;
+var totalDue = costOfVetVisit + vacinationFee;
+console.log(totalDue);
+
+var priceOfMeal = 72.85;
+var discountCoupon = 10;
+var totalDue = priceOfMeal - discountCoupon;
+console.log(totalDue)
+
+var num = 63;
+if(num % 2 == 0){
+    console.log("THe number " + num + " is even!")
+}
+else{
+    console.log("THe number " + num + " is odd!")
+}
---
+++ Lecturedemos/2-8.js
@@ -0,0 +1,22 @@
+"use strict"
+//NaN
+var num;
+num = num + 1;
+console.log(num);
+
+//Infinity
+var num1 = 60;
+var num2 = 0;
+
+var answer = num1 / num2;
+
+console.log(answer);
+//Infinity
+var answer2 = answer + 1;
+
+console.log(answer2);
+
+var example = 9;
+console.log(example);
+var example2 = example + 1;
+console.log(example2);
